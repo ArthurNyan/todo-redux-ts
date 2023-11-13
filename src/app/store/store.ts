1beb3f68979cad48a1127f5f99a60ef0593c93eb
Нахатакян Артур Романович <artoim333@gmail.com>
2023-11-14 02:58:34 +0300

    init userSlice in redux

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoSlice from './todoSlice';
+import userSlice from './userSlice';
 
 export const store = configureStore({
     reducer: {
         todo: todoSlice,
+        user: userSlice,
     }
 });
 
