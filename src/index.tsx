092ca1ad8778f79e0ead58656dd2e0b5c53ec26b
Нахатакян Артур Романович <artoim333@gmail.com>
2023-11-14 21:58:50 +0300

    feat: add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import { Provider } from 'react-redux';
 import { store } from './app/store/store';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/App';
+import './app/firebase/firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Provider store={store}><App /></Provider>);
